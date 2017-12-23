42a62fd57386a21aa818c08f1e3755849fe34a04
David Greene <david@securelink.com>
2017-12-22 20:53:03 -0600

    Pre-redux

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@
 import React, {Component} from 'react';
 import { withGoogleMap, GoogleMap, Polygon } from 'react-google-maps';
 import gis from './gis.json';
+import schools from './schools.json';
 import MarkerWithLabel from 'react-google-maps/lib/components/addons/MarkerWithLabel';
+import _ from 'lodash';
 
-const CENTER_X_OFFSET = -0.004;
+const CENTER_X_OFFSET = -0.002;
+
+const YEARS = ['2017', '2018', '2019', '2020', '2021', '2020', '2021', '2022'];
 
 const center = (coords) => {
     let bounds = new google.maps.LatLngBounds();
@@ -26,10 +30,54 @@ const GoogleMapApp = withGoogleMap((props) =>
             {props.children}
         </GoogleMap>);
 
+const Schools = (props) => Object.keys(props.schools).map((s) =>
+        <MarkerWithLabel
+            key={s}
+            position={props.schools[s].position}
+            labelAnchor={new google.maps.Point(0, 0)}
+            labelStyle={{backgroundColor: "#ccc", fontSize: 10, padding: 2 }}
+            icon='pixel-trans.gif'
+        >
+            <div>props.schools[s].label</div>
+        </MarkerWithLabel>
+    );
+
+const PlanningUnits = (props) => Object.keys(gis).map(pu => {
+    return <div key={pu}>
+        <Polygon
+            path={gis[pu]}
+            options={{
+                fillColor: '#000',
+                fillOpacity: 0.2
+            }}
+            onClick={function(){this.setOptions({fillColor: 'red'})}}
+        />
+        <MarkerWithLabel
+            position={center(gis[pu])}
+            labelAnchor={new google.maps.Point(0, 0)}
+            labelStyle={{backgroundColor: "#ccc", fontSize: 10, padding: 2 }}
+            icon='pixel-trans.gif'
+        >
+            <div>{pu.substring(2)}</div>
+        </MarkerWithLabel>
+    </div>;
+});
+
+const Years = (props) =>  props.years.map((y) =>
+    <div>
+        <span style={{width: 100, textAlign: 'right', marginRight: 20}}>{y}:</span>
+        <input key={y} style={{width: '5em', backgroundColor: '#EEE'}} type='text' disabled  value={_.sum(props.planningUnits, (pu) => props.planningUnits[y])} />
+    </div>);
+
+const Controls = (props) => Object.keys(props.schools).map(school => <div>
+    <h3>{props.schools[school].label}</h3>
+    <Years years={props.years} planningUnits={props.schools[school].pus} />
+</div>);
+
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {width: '0', height: '0' };
+        this.state = {width: '0', height: '0', schools: schools, years: YEARS };
     }
 
     componentDidMount() {
@@ -44,33 +92,19 @@ class App extends Component {
     updateWindowDimensions = () => this.setState({width: window.innerWidth, height: window.innerHeight});
 
     render() {
-        return <div><GoogleMapApp
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: this.state.height, width: this.state.width - 300 }} />}
-            mapElement={<div style={{ height: `100%` }}
-            />}
-        >
-            {Object.keys(gis).map(pu => {
-                return <div key={pu}>
-                    <Polygon
-                        path={gis[pu]}
-                        options={{
-                            fillColor: '#000',
-                            fillOpacity: 0.2
-                        }}
-                        onClick={function(){this.setOptions({fillColor: 'red'})}}
-                    />
-                    <MarkerWithLabel
-                        position={center(gis[pu])}
-                        labelAnchor={gis[pu][0]}
-                        labelStyle={{backgroundColor: "#ccc", fontSize: 10, padding: 2 }}
-                        icon='pixel-trans.gif'
-                    >
-                        <div>{pu}</div>
-                    </MarkerWithLabel>
-                </div>;
-            })}
-        </GoogleMapApp><div style={{ padding: 10, position: 'absolute', top: 0, left: this.state.width - 300, width: 300, height: this.state.height}}>Hi</div></div>
+        let activeSchools = _.filter(this.state.schools, (s) => s.active);
+        return <div>
+            <GoogleMapApp
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: this.state.height, width: this.state.width - 300 }} />}
+                mapElement={<div style={{ height: `100%` }} />} >
+                <PlanningUnits />
+                <Schools schools={this.state.schools}  />
+            </GoogleMapApp>
+            <div style={{ padding: 10, position: 'absolute', top: 0, left: this.state.width - 300, width: 300, height: this.state.height}}>
+                <Controls schools={this.state.schools} years={this.state.years}/>
+            </div>
+        </div>
     }
 }
 
